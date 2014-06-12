d7eb43d18f77453786e44570f6e1c9ec6bae1658
supercclank <supercclank@gmail.com>
2014-06-11 20:56:21 -0500

    Red fireball for game
    Red fireball enemy for game.

---
+++ JetPac/redfireball.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 redfireball redfireball.png 
+ * Time-stamp: Saturday 04/12/2014, 23:42:28
+ * 
+ * Image Information
+ * -----------------
+ * redfireball.png 16@11
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Python's a drop-in replacement for BASIC in the sense that Optimus Prime is a drop-in replacement for a truck.
+ *  - Cory Dodt
+ * 
+ * Living with a commitment to excellence shall take you far.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef REDFIREBALL_BITMAP_H
+#define REDFIREBALL_BITMAP_H
+
+extern const unsigned short redfireball[176];
+#define REDFIREBALL_SIZE 176
+#define REDFIREBALL_WIDTH 16
+#define REDFIREBALL_HEIGHT 11
+
+#endif
+
