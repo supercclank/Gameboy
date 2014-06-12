d7520d4c3695d4dfa15f69f2d35fb0ef7187d595
supercclank <supercclank@gmail.com>
2014-06-11 20:57:44 -0500

    Win screen.
    Screen displayed when player completes the game

---
+++ JetPac/winscreen.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 winscreen winscreen.png 
+ * Time-stamp: Monday 04/14/2014, 00:43:41
+ * 
+ * Image Information
+ * -----------------
+ * winscreen.png 240@160
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Perfection [in design] is achieved, not when there is nothing more to add, but when there is nothing left to take away.
+ *  - Antoine de Saint-Exupéry
+ * 
+ * You should be able to undertake and complete anything.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef WINSCREEN_BITMAP_H
+#define WINSCREEN_BITMAP_H
+
+extern const unsigned short winscreen[38400];
+#define WINSCREEN_SIZE 38400
+#define WINSCREEN_WIDTH 240
+#define WINSCREEN_HEIGHT 160
+
+#endif
+
