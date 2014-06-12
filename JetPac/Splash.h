da2d52a248ba69bbfd11d0e4204435a48feabb71
supercclank <supercclank@gmail.com>
2014-06-11 20:57:29 -0500

    Slash screen
    Screen displayed when game opens

---
+++ JetPac/Splash.h
@@ -0,0 +1,28 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 Splash jetpac_title.png 
+ * Time-stamp: Saturday 04/12/2014, 18:32:55
+ * 
+ * Image Information
+ * -----------------
+ * jetpac_title.png 240@160
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Hardware: The parts of a computer system that can be kicked.
+ * 
+ * Success is going from failure to failure without loss of enthusiasm.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef SPLASH_BITMAP_H
+#define SPLASH_BITMAP_H
+
+extern const unsigned short jetpac_title[38400];
+#define JETPAC_TITLE_SIZE 38400
+#define JETPAC_TITLE_WIDTH 240
+#define JETPAC_TITLE_HEIGHT 160
+
+#endif
+
