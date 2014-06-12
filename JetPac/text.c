a580ba7a13ccc1e0d679e3e86c194ef23e33cc1e
supercclank <supercclank@gmail.com>
2014-06-11 20:57:19 -0500

    Text library for game.
    Class that contains useful functions for writing text to screen

---
+++ JetPac/text.c
@@ -0,0 +1,31 @@
+#include "mylib.h"
+
+#include "text.h"
+
+void drawChar(int row, int col, char ch, unsigned short color)
+{
+	int r,c;
+	for(r=0; r<8; r++)
+	{
+		for(c=0; c<6; c++)
+		{
+			if(fontdata_6x8[OFFSET(r, c, 6) + ch*48])
+			{
+				setPixel(row+r, col+c, color);
+			} 
+		}
+	}
+}
+
+void drawString(int row, int col,  char *str, unsigned short color)
+{
+	while(*str)
+	{
+		drawChar(row, col, *str++, color);
+		col += 6;
+	}
+}
+	
+		
+	
+
