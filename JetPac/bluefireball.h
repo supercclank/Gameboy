e32a6baa8babd9a0a20939e4467af77310fe03c8
supercclank <supercclank@gmail.com>
2014-06-11 20:53:16 -0500

    blue fire ball
    Blue fireball enemy used in the game

---
+++ JetPac/bluefireball.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 bluefireball bluefireball.png 
+ * Time-stamp: Saturday 04/12/2014, 23:42:17
+ * 
+ * Image Information
+ * -----------------
+ * bluefireball.png 16@11
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Python's a drop-in replacement for BASIC in the sense that Optimus Prime is a drop-in replacement for a truck.
+ *  - Cory Dodt
+ * 
+ * People find it difficult to resist your persuasive manner.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef BLUEFIREBALL_BITMAP_H
+#define BLUEFIREBALL_BITMAP_H
+
+extern const unsigned short bluefireball[176];
+#define BLUEFIREBALL_SIZE 176
+#define BLUEFIREBALL_WIDTH 16
+#define BLUEFIREBALL_HEIGHT 11
+
+#endif
+
