dad7cd699b61066f2ecff31f9e7e147e926a6cb9
supercclank <supercclank@gmail.com>
2014-06-11 20:55:59 -0500

    Pink fireball for game
    Pink fireball enemy in game

---
+++ JetPac/pinkfireball.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 pinkfireball pinkfireball.png 
+ * Time-stamp: Saturday 04/12/2014, 23:45:25
+ * 
+ * Image Information
+ * -----------------
+ * pinkfireball.png 16@11
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Python's a drop-in replacement for BASIC in the sense that Optimus Prime is a drop-in replacement for a truck.
+ *  - Cory Dodt
+ * 
+ * You have a deep interest in all that is artistic.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef PINKFIREBALL_BITMAP_H
+#define PINKFIREBALL_BITMAP_H
+
+extern const unsigned short pinkfireball[176];
+#define PINKFIREBALL_SIZE 176
+#define PINKFIREBALL_WIDTH 16
+#define PINKFIREBALL_HEIGHT 11
+
+#endif
+
