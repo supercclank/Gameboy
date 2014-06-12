fa270009011e2651d10883d950c3cf0843da2a92
supercclank <supercclank@gmail.com>
2014-06-11 20:53:00 -0500

    Background for game
    background used throughout the game

---
+++ JetPac/background.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 background background.png 
+ * Time-stamp: Saturday 04/12/2014, 23:18:31
+ * 
+ * Image Information
+ * -----------------
+ * background.png 240@160
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Python's a drop-in replacement for BASIC in the sense that Optimus Prime is a drop-in replacement for a truck.
+ *  - Cory Dodt
+ * 
+ * You are soon going to change your present line of work.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef BACKGROUND_BITMAP_H
+#define BACKGROUND_BITMAP_H
+
+extern const unsigned short background[38400];
+#define BACKGROUND_SIZE 38400
+#define BACKGROUND_WIDTH 240
+#define BACKGROUND_HEIGHT 160
+
+#endif
+
