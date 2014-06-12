aa83c2d6c4f83415ce955262619b0bb6af1b6eda
supercclank <supercclank@gmail.com>
2014-06-11 20:56:48 -0500

    Ship part
    Ship part 3

---
+++ JetPac/shippart3.h
@@ -0,0 +1,28 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 shippart3 ship_part3.png 
+ * Time-stamp: Saturday 04/12/2014, 19:20:12
+ * 
+ * Image Information
+ * -----------------
+ * ship_part3.png 18@18
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Hardware: The parts of a computer system that can be kicked.
+ * 
+ * Listen not to vain words of empty tongue.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef SHIPPART3_BITMAP_H
+#define SHIPPART3_BITMAP_H
+
+extern const unsigned short ship_part3[324];
+#define SHIP_PART3_SIZE 324
+#define SHIP_PART3_WIDTH 18
+#define SHIP_PART3_HEIGHT 18
+
+#endif
+
