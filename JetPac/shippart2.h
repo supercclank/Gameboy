b8c8c7a38267c193e816e38efa21f6b4b630d749
supercclank <supercclank@gmail.com>
2014-06-11 20:56:38 -0500

    Ship part
    Ship part 2

---
+++ JetPac/shippart2.h
@@ -0,0 +1,28 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 shippart2 ship_part2.png 
+ * Time-stamp: Saturday 04/12/2014, 19:10:07
+ * 
+ * Image Information
+ * -----------------
+ * ship_part2.png 11@17
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Hardware: The parts of a computer system that can be kicked.
+ * 
+ * All the troubles you have will pass away very quickly.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef SHIPPART2_BITMAP_H
+#define SHIPPART2_BITMAP_H
+
+extern const unsigned short ship_part2[187];
+#define SHIP_PART2_SIZE 187
+#define SHIP_PART2_WIDTH 11
+#define SHIP_PART2_HEIGHT 17
+
+#endif
+
