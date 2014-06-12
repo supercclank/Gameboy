102c695e5085e5e63e2dabc16e910d16a84d7feb
supercclank <supercclank@gmail.com>
2014-06-11 20:54:53 -0500

    Lose screen
    Screen displayed when player runs out of lives

---
+++ JetPac/losescreen.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 losescreen losescreen.png 
+ * Time-stamp: Monday 04/14/2014, 00:53:03
+ * 
+ * Image Information
+ * -----------------
+ * losescreen.png 240@160
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Perfection [in design] is achieved, not when there is nothing more to add, but when there is nothing left to take away.
+ *  - Antoine de Saint-Exupéry
+ * 
+ * Rest has a peaceful effect on your physical and emotional health.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef LOSESCREEN_BITMAP_H
+#define LOSESCREEN_BITMAP_H
+
+extern const unsigned short losescreen[38400];
+#define LOSESCREEN_SIZE 38400
+#define LOSESCREEN_WIDTH 240
+#define LOSESCREEN_HEIGHT 160
+
+#endif
+
