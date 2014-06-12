f24a1845b8797278d390372ff536c863ea6451f3
supercclank <supercclank@gmail.com>
2014-06-11 20:54:05 -0500

    Platform used in game.
    Platform used in game to rest ship part 2 on.

---
+++ JetPac/gplatform2.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 gplatform2 gplatform2.png 
+ * Time-stamp: Sunday 04/13/2014, 01:52:03
+ * 
+ * Image Information
+ * -----------------
+ * gplatform2.png 49@8
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Python's a drop-in replacement for BASIC in the sense that Optimus Prime is a drop-in replacement for a truck.
+ *  - Cory Dodt
+ * 
+ * You are open-minded and quick to make new friends.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef GPLATFORM2_BITMAP_H
+#define GPLATFORM2_BITMAP_H
+
+extern const unsigned short gplatform2[392];
+#define GPLATFORM2_SIZE 392
+#define GPLATFORM2_WIDTH 49
+#define GPLATFORM2_HEIGHT 8
+
+#endif
+
