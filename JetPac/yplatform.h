a73d212545e1b82fa6e63b3f4690c873e7890a02
supercclank <supercclank@gmail.com>
2014-06-11 20:52:44 -0500

    Platform for game
    Platform used to rest ship parts on

---
+++ JetPac/yplatform.h
@@ -0,0 +1,29 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 yplatform yplatform.png 
+ * Time-stamp: Sunday 04/13/2014, 01:55:58
+ * 
+ * Image Information
+ * -----------------
+ * yplatform.png 255@8
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Python's a drop-in replacement for BASIC in the sense that Optimus Prime is a drop-in replacement for a truck.
+ *  - Cory Dodt
+ * 
+ * Say hello to others. You will have a happier day.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef YPLATFORM_BITMAP_H
+#define YPLATFORM_BITMAP_H
+
+extern const unsigned short yplatform[2040];
+#define YPLATFORM_SIZE 2040
+#define YPLATFORM_WIDTH 255
+#define YPLATFORM_HEIGHT 8
+
+#endif
+
